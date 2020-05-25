5af7adf1aa25776d1d76c329f24c03d937142903
Alexey <shtirlits1972@gmail.com>
2020-05-26 01:08:24 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Auction.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Auction.Controllers
+{
+    [Authorize]
+    [Authorize(Roles = "админ")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            List<Users> list = UsersCrud.GetAll();
+            return Json(list);
+        }
+
+        [HttpPost]
+        public JsonResult Add(Users model)
+        {
+            model = UsersCrud.Add(model);
+            return Json(model);
+        }
+
+        [HttpPost]
+        public void Edit(Users model)
+        {
+            UsersCrud.Edit(model);
+        }
+
+        [HttpPost]
+        public void Del(int Id)
+        {
+            UsersCrud.Del(Id);
+        }
+    }
+}
